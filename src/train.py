e2680fcf3843725b9519a1ad71771841c93b778e
Bhavesh2123 <146112682+Bhavesh2123@users.noreply.github.com>
2026-02-11 23:19:03 +0530

    train.py updated

--- src/train.py
+++ src/train.py
@@ -2,3 +2,6 @@ from dataset import load_data
 from model import build_model
 from sklearn.model_selection import train_test_split
 import numpy as np
+X,y =load_data
+X=X[..., np.newaxis]
+X_train, X_temp, y_train, y_temp=train_test_split()
